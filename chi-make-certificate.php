95b8599d75ef11632e1b8cf7964df9f0723c8a6e
Merchans <addmarkovic@gmail.com>
2021-05-06 14:18:32 +0200

    add time format for certificat

--- chi-make-certificate.php
+++ chi-make-certificate.php
@@ -1,82 +1,257 @@
 <?php
 
-/**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://github.com/Merchans/chi-make-certificate
- * @since             1.0.0
- * @package           Chi_Make_Certificate
- *
- * @wordpress-plugin
- * Plugin Name:       CHI MAKE CERTIFICATE 
- * Plugin URI:        chi-make-certificate
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Richard Markovič
- * Author URI:        https://github.com/Merchans/chi-make-certificate
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       chi-make-certificate
- * Domain Path:       /languages
- */
+	/**
+	 * The plugin bootstrap file
+	 *
+	 * This file is read by WordPress to generate the plugin information in the plugin
+	 * admin area. This file also includes all of the dependencies used by the plugin,
+	 * registers the activation and deactivation functions, and defines a function
+	 * that starts the plugin.
+	 *
+	 * @link              https://github.com/Merchans/chi-make-certificate
+	 * @since             1.0.0
+	 * @package           Chi_Make_Certificate
+	 *
+	 * @wordpress-plugin
+	 * Plugin Name:       CHI Make Certificate
+	 * Plugin URI:        chi-make-certificate
+	 * Description:       Make PDF Certificate
+	 * Version:           1.0.0
+	 * Author:            Richard Markovič
+	 * Author URI:        https://github.com/Merchans/chi-make-certificate
+	 * License:           GPL-2.0+
+	 * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+	 * Text Domain:       chi-make-certificate
+	 * Domain Path:       /languages
+	 */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+// Add autoloader
+	require __DIR__ . '/vendor/autoload.php';
+
+
+	// If this file is called directly, abort.
+	if ( ! defined( 'WPINC' ) ) {
+		die;
+	}
+
+	/**
+	 * Currently plugin version.
+	 * Start at version 1.0.0 and use SemVer - https://semver.org
+	 * Rename this for your plugin and update it as you release new versions.
+	 */
+	define( 'CHI_MAKE_CERTIFICATE_VERSION', '1.0.0' );
+
+	function make_pdf_certificat( $name = null, $course_name = null, $credit = 3, $professional_guarantor = null, $CKL_id = null, $created_at = null, $CKL_register = null ) {
+
+		$basic_path = dirname( __FILE__, "1" );
+		$html       = '
+	<style>
+
+		body {
+			font-family: "Source Sans Pro", sans-serif;
+			margin: 0 auto;
+			color: #1B1717;
+			background: white;
+		}
+
+		.a4 {
+			height: 842px;
+			width: 595px;
+			margin: 0 auto;
+			background: white;
+			position: relative;
+		}
+
+		.badge {
+			width: 100px;
+			position: absolute;
+			top: -60px;
+			left: 4px;
+		}
+
+		.stamp {
+			width: 150px;
+			align-self: flex-end;
+			z-index: 10;
+		}
+
+		header img {
+			width: 595px;
+			padding: 0;
+			display: block;
+			margin: 0 auto;
+		}
+		header  {
+			text-align: center;
+		}
+
+footer{
+		margin: 4.7rem 0rem;
+		}
+
+		footer p {
+
+    font-size: 1.5rem;
+    line-height: 2rem;
+}
+		.pre-footer {
+			position: absolute;
+			width: 100%;
+			bottom: 222px;
+		}
+		section.text {
+			display: flex;
+			flex-direction: column;
+		}
+		h1 {
+			font-size: 2.5rem;
+			text-align: center;
+			font-weight: normal;
+			line-height: 4rem;
+			color:#CC3433;
+		}
+		h2 {
+    font-size: 2rem;
+    text-align: center;
+}
+small
+{
+			text-align: center;
+    font-size: 1.2rem;
+		}
+		p {
+			text-align: center;
+    font-size: 1.5rem;
+    margin-top: 1.4rem;
+		}
+		.uppercase{
+			text-transform: uppercase;
+			text-align: center;
+		}
+		h3 {
+		font-size: 2rem;
+		margin-bottom: 1rem;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'CHI_MAKE_CERTIFICATE_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-chi-make-certificate-activator.php
- */
-function activate_chi_make_certificate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-chi-make-certificate-activator.php';
-	Chi_Make_Certificate_Activator::activate();
+		h4 {
+		font-size: 1.7rem;
+		margin-bottom: 0.5rem;
 }
+		h1.uppercase {
+    line-height: 3rem;
+    font-weight: bold;
+    font-size: 3.3rem;
+    margin-bottom: 0;
+		}
+	</style>
+</head>
+<body>
+<section class="a4">
+	<header>
+		<img src="' . $basic_path . '/img/certificat-header.png" alt="CHI zahlavi">
+		<small><em>Czech Health Interactive, Národní třída 58/32, 110 00 Praha 1</em></small>
+	</header>
+	<section class="text">
+		<h1 class="uppercase">
+			Potvrzení o účasti <br><br>na vzdělávací akci
+		</h1>
+		<p>
+			<em>pořádané v rámci celoživotního vzdělávání lékařů <br> dle stavovského předpisu ČLK č.16</em>
+		</p>
+		<br>
+		<h2><em>' . $name . '</em></h2>
+		<br>
+		<br>
+		<h3 class="uppercase">' . $course_name . '</h3>
+		<p>e-learningový kurz na portálu kongresonline.cz</p>
+		<h4 class="uppercase">počet přidělených kreditů: ' . $credit . '</h4>
+	</section>
+	<br>
+	<footer>
+		<p>
+			Datum absolvování testu: ' . $created_at . ' <br>
+			V centrálním registru je akce vedena pod číslem: '. $CKL_register .'<br>
+			Odborný garant: ' . $professional_guarantor . '
+		</p>
+	</footer>
+</section>
+</body>
+</html>';
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-chi-make-certificate-deactivator.php
- */
-function deactivate_chi_make_certificate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-chi-make-certificate-deactivator.php';
-	Chi_Make_Certificate_Deactivator::deactivate();
-}
+		$mpdf = new \Mpdf\Mpdf();
 
-register_activation_hook( __FILE__, 'activate_chi_make_certificate' );
-register_deactivation_hook( __FILE__, 'deactivate_chi_make_certificate' );
 
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-chi-make-certificate.php';
+		$mpdf->SetTitle( "CERTIFIKÁT-" . $name . "" );
+		$mpdf->SetAuthor( 'Autor' );
+		$mpdf->SetCreator( 'Vytvořil' );
+		$mpdf->SetSubject( 'Předmět' );
+		$mpdf->SetKeywords( 'Klíčová slova' );
+		$mpdf->WriteHTML( $html );
+		$filename = $CKL_id . "-certifikat.pdf";
+		$path     = wp_upload_dir()['basedir'];
+		$mpdf->Output( $path . '/certifikaty/' . $filename, "F" );
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_chi_make_certificate() {
 
-	$plugin = new Chi_Make_Certificate();
-	$plugin->run();
+	}
 
-}
-run_chi_make_certificate();
+//	http://localhost:8888/Users/merchans/www/WordpPress/KongresOnline/wp-content/uploads/certifikaty/122222-certifikat.pdf
+//	http://localhost:8888/WordpPress/KongresOnline/wp-content/uploads/2021/01/covid_vakcina.jpg
+
+	if ( isset( $_GET["make_pdf"] ) ) {
+
+		$path = wp_upload_dir()['baseurl'];
+
+		$CKL_id = $_GET["ckl_id"];
+
+
+		// The location of the PDF file
+		// on the server
+		$filename = $path . '/certifikaty/' . $CKL_id . '-certifikat.pdf';
+
+		// Header content type
+		header( "Content-type: application/pdf" );
+
+		header( "Content-Length: " . filesize( $filename ) );
+
+		// Send the file to the browser.
+		readfile( $filename );
+	}
+
+	function make_pdf_certificate( $atts ) {
+
+
+		$path = wp_upload_dir()['baseurl'];
+
+//		http://localhost:8888/Users/merchans/www/WordpPress/KongresOnline/wp-content/uploads/certifikaty/122222-certifikat.pdf
+
+
+
+		$filename = $path . '/certifikaty/' . $atts['ckl'] . '-certifikat.pdf';
+
+		$ckl = $atts['ckl'];
+
+		$date = new DateTime();
+		$atts = shortcode_atts( array(
+			'ckl'              => '',
+			'ckl_registr'     => '',
+			'jmeno_doktora'    => '',
+			'jmeno_kurzu'      => '',
+			'kredity'          => 3,
+			'garant_kurzu'     => '',
+			'datum_vystavenia' => $date->format("j. n. Y"),
+			'nazev_tlacitka'   => 'Stáhnout certifikát',
+			'class'            => 'btn btn-success',
+		), $atts );
+
+
+//		$name = null, $course_name = null, $credit = 3, $professional_guarantor = null, $CKL_id = null, $created_at = null
+
+
+		make_pdf_certificat( $atts['jmeno_doktora'], $atts['jmeno_kurzu'], $atts['kredity'], $atts['garant_kurzu'], $atts['ckl'], $atts['datum_vystavenia'], $atts['ckl_registr'] );
+
+		return '<a target="_blank" class="' . esc_attr( $atts['class'] ) . '" href="' . $path . '/certifikaty/' . $atts['ckl'] . '-certifikat.pdf' . '">' . $atts['nazev_tlacitka'] . '</a>';
+
+	}
+
+	add_shortcode( 'certifikat', 'make_pdf_certificate' );
+
